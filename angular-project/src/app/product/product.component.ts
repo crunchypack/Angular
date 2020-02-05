23aec80c1c36b0c0f665a21fc951645d21c415b7
Simon <simon.wolf.lobo@gmail.com>
2020-02-04 20:47:17 -0500

    finished refresh, Angular in a nutshell

--- angular-project/src/app/product/product.component.ts
+++ angular-project/src/app/product/product.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { ProductsService } from "../products.service";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
 export class ProductComponent implements OnInit {
+  @Input() productName: string;
+  @Output() productClicked = new EventEmitter();
+  constructor(private productsService: ProductsService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onClicked() {
+    // this.productClicked.emit();
+    this.productsService.deleteProduct(this.productName);
   }
-
 }
--- angular-project/src/app/products.component.ts
+++ angular-project/src/app/products.component.ts
@@ -1,19 +1,42 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ProductsService } from "./products.service";
 
 @Component({
   selector: "app-products",
   templateUrl: "./products.component.html"
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   productName = "A book";
   isDisabled = true;
-  products = ["A book", "A tree"];
-  constructor() {
+  products = [];
+  private productsSubcription: Subscription;
+
+  constructor(private productsService: ProductsService) {
     setTimeout(() => {
       this.isDisabled = false;
     }, 3000);
   }
-  onAddProduct() {
-    this.products.push(this.productName);
+  ngOnInit() {
+    this.products = this.productsService.getProducts();
+    this.productsSubcription = this.productsService.productsUpdated.subscribe(
+      () => {
+        this.products = this.productsService.getProducts();
+      }
+    );
+  }
+  onAddProduct(form) {
+    if (form.valid) {
+      // this.products.push(form.value.productName);
+      this.productsService.addProduct(form.value.productName);
+    }
+
+    // this.products.push(this.productName);
+  }
+  onRemoveProduct(productName: string) {
+    this.products = this.products.filter(p => p !== productName);
+  }
+  ngOnDestroy() {
+    this.productsSubcription.unsubscribe();
   }
 }
